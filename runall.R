2ba696ed1ed8e0773a81dce2018125e6362b914f
graphdr <graphdoctor@gmail.com>
2016-09-03 14:03:02 -0400

    slides 001 002

--- runall.R
+++ runall.R
@@ -2,12 +2,15 @@ library(stringr)
 library(knitr)
 
 # render Rnw for slides, uses knitr::knit2pdf (takes time)
-# Rnw_slide_scripts <- list.files(path = "week_00"
-# 															 , pattern = "\\.Rnw$"
-# 															 , full.names = TRUE
-# )
-# tex_slide_files <- str_replace(Rnw_slide_scripts, 'Rnw$', 'tex')
+Rnw_slide_scripts <- list.files(path = "week_00"
+															 , pattern = "\\.Rnw$"
+															 , full.names = TRUE
+)
+tex_slide_files <- str_replace(Rnw_slide_scripts, 'Rnw$', 'tex')
+# knit2pdf takes time and overwrites existing slide-pdfs
 # Map(function(x, y) knit2pdf(input = x, output = y), Rnw_slide_scripts, tex_slide_files)
+# delete the text file byproducts
+sapply(tex_slide_files, function(x) unlink(x))
 
 # delete R byproducts
 unlink(".Rhistory")
@@ -27,11 +30,6 @@ unlink("*/*.aux")
 unlink("*/*.out")
 unlink("*/*.vrb")
 
-
-
-
-
-sapply(tex_slide_files, function(x) unlink(x))
 
 
 
