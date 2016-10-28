c7c3ea187fc07d4562068c940cc4ab361a0460ff
graphdr <graphdoctor@gmail.com>
2016-10-28 17:05:07 -0400

    project 1 wrap-up

--- runall.R
+++ runall.R
@@ -3,13 +3,8 @@ library(knitr)
 library(rmarkdown)
 library(readr)
 
-
-# array of folders to search for Rmd and Rnw filed to compile
-compile_these <- c("cm") # "cm", "print_pages", "slides"
-
-# delete old md files before rendering Rmd to md
-# delete_these <- paste0(compile_these, "/*.md")
-# sapply(delete_these, function(x) unlink(x))
+# render all Rmd files in CM folder
+compile_these <- c("cm")
 
 # compile Rmd to github variant md
 Rmd_scripts <- list.files(path = compile_these
@@ -18,18 +13,23 @@ Rmd_scripts <- list.files(path = compile_these
 )
 sapply(Rmd_scripts, function(x) render(x))
 
-# or just do one file
-# library(rmarkdown)
-# render("cm/cm048_data-joining.Rmd")
 
-# render Rnw for slides, uses knitr::knit2pdf (takes time)
-Rnw_slide_scripts <- list.files(path = compile_these
-															 , pattern = "\\.Rnw$"
-															 , full.names = TRUE
-)
-tex_slide_files <- str_replace(Rnw_slide_scripts, 'Rnw$', 'tex')
-# knit2pdf takes time and overwrites existing slide-pdfs
-# Map(function(x, y) knit2pdf(input = x, output = y), Rnw_slide_scripts, tex_slide_files)
-# delete the text file byproducts
-sapply(tex_slide_files, function(x) unlink(x))
+
+
+
+
+# # or just do one file
+# # library(rmarkdown)
+# # render("cm/cm048_data-joining.Rmd")
+#
+# # render Rnw for slides, uses knitr::knit2pdf (takes time)
+# Rnw_slide_scripts <- list.files(path = compile_these
+# 															 , pattern = "\\.Rnw$"
+# 															 , full.names = TRUE
+# )
+# tex_slide_files <- str_replace(Rnw_slide_scripts, 'Rnw$', 'tex')
+# # knit2pdf takes time and overwrites existing slide-pdfs
+# # Map(function(x, y) knit2pdf(input = x, output = y), Rnw_slide_scripts, tex_slide_files)
+# # delete the text file byproducts
+# sapply(tex_slide_files, function(x) unlink(x))
 
